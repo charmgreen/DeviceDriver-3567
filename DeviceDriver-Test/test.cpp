df2f4ddcb01d1759243e1ff682ecc75d8305ca60
jihun.you <bluegil92@naver.com>
2024-07-05 09:06:24 +0900

    Let's start Device Driver KATA

---
+++ DeviceDriver-Test/test.cpp
@@ -0,0 +1,12 @@
+//#include "pch.h"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "../DeviceDriver_KATA/DeviceDriver.cpp"
+
+using namespace std;
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
